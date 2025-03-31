aad13e6b0c31a57afa995f46d205745dd89801b9
Breno <brnodionisio23@gmail.com>
2025-03-31 20:55:22 -0300

    Configurações Iniciais do Spring

---
+++ src/test/java/com/brenodev/Pilates/PilatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenodev.Pilates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
