0571c519d999873c05dfbade268775c79868e5ca
Breno <brnodionisio23@gmail.com>
2025-04-01 11:02:00 -0300

    Upgrade na classe Person

--- src/main/java/com/brenodev/Pilates/model/Person.java
+++ src/main/java/com/brenodev/Pilates/model/Person.java
@@ -1,8 +1,12 @@
 package com.brenodev.Pilates.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.time.LocalDate;
 
@@ -10,11 +14,22 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public abstract class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
     private String name;
+    @NonNull
     private String lastName;
+    @NonNull
     private String cpf;
+    @NonNull
     private String phone;
+    @NonNull
     private String mail;
+    @NonNull
     private LocalDate birth;
 
 }
