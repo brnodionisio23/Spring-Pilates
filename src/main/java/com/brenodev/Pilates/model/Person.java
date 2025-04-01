647b6b77bf3c9e833a8a102cfcca3d43563cb7cd
Breno <brnodionisio23@gmail.com>
2025-04-01 11:27:48 -0300

    update Person

--- src/main/java/com/brenodev/Pilates/model/Person.java
+++ src/main/java/com/brenodev/Pilates/model/Person.java
@@ -1,35 +1,47 @@
 package com.brenodev.Pilates.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public abstract class Person {
+@MappedSuperclass
+public abstract class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NonNull
+    @Column(name = "nome")
     private String name;
+
     @NonNull
+    @Column(name = "Sobrenome")
     private String lastName;
+
     @NonNull
+    @Column(name = "CPF")
     private String cpf;
+
     @NonNull
+    @Column(name = "Telefone")
     private String phone;
+
     @NonNull
+    @Column(name = "Email")
     private String mail;
+
     @NonNull
+    @Column(name = "Nascimento")
     private LocalDate birth;
 
 }
